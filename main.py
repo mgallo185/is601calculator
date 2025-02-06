18588dd5a6c7807b53d9749ee6d5c7d1f4d998c6
mgallo185 <mgallo185@gmail.com>
2025-02-06 15:31:42 -0500

    added operation class and static methods for each operation

---
+++ main.py
@@ -0,0 +1,8 @@
+from calculator.operation import Calculator
+
+# So, what this line means is: "If you're running this program directly (not as part of another program), 
+# then go ahead and start the calculator."
+if __name__ == "__main__":
+    # Now, we use the calculator tool we got earlier. This will start the calculator, which is a program 
+    # that keeps running and doing math based on what we tell it.
+    Calculator()
